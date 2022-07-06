1a08260e3873d7984f17e57894bde767121c50c0
Guilherme Daldim <gdaldims@gmail.com>
2022-07-06 15:48:21 -0300

    Create Micro Front App

---
+++ micro_app_login/test/micro_app_login_test.dart
@@ -0,0 +1,3 @@
+void main() {
+  //Write tests
+}
---
+++ micro_core/lib/app/micro_core_utils.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/widgets.dart';
+
+typedef WidgetBuildersArgs = Widget Function(BuildContext context, Object args);
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey();
