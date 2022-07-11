81583097cfae302a77b68544b5c2337fe093bb79
Guilherme Daldim <gdaldims@gmail.com>
2022-07-11 14:41:17 -0300

    Deleting folders

--- micro_app_login/test/micro_app_login_test.dart
+++
@@ -1,3 +0,0 @@
-void main() {
-  //Write tests
-}
--- micro_core/lib/app/micro_core_utils.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/widgets.dart';
-
-typedef WidgetBuildersArgs = Widget Function(BuildContext context, Object args);
-final GlobalKey<NavigatorState> navigatorKey = GlobalKey();
